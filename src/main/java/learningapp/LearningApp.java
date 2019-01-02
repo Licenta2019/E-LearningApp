0565327f503721ad8e894da13ac8eb3035a5a56f
JohnPaul97 <cus.paul@yahoo.com>
2019-01-02 16:34:04 +0200

    restructure project, add gitignore and gradle dependencies

---
+++ src/main/java/learningapp/LearningApp.java
@@ -0,0 +1,13 @@
+package learningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningApp.class, args);
+    }
+
+}
