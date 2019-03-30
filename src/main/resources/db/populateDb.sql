8ffa3cc6c2a1450b81b747af5840d62dcb6483d1
JohnPaul <cus.paul@yahoo.com>
2019-03-30 13:02:53 +0200

    [LAPP-02] add populate db script

---
+++ src/main/resources/db/populateDb.sql
@@ -0,0 +1,6 @@
+insert into application_user values ('15ba3454-65e2-439c-8519-9ba135cf97b9','cus.paul@yahoo.com','123','STUDENT','paul');
+
+insert into subject values('15ba3454-65e2-439c-8519-9ba135cf97b1','MAP');
+
+insert into topic values('15ba3454-65e2-439c-8519-9ba135cf97b2','Java','15ba3454-65e2-439c-8519-9ba135cf97b1');
+insert into topic values('15ba3454-65e2-439c-8519-9ba135cf97b3','C#','15ba3454-65e2-439c-8519-9ba135cf97b1');
